60ff1c5ee88f7a15c30e0eede40b02de2f7cce27
Jordan <jordan@charloe.com>
2021-02-23 12:22:31 -0500

    stack and queues section added

---
+++ Stacks and Queues/Balanced Brackets/BalancedBrackets.py
@@ -0,0 +1,144 @@
+import math
+import os
+import random
+import re
+import sys
+import bisect
+import requests
+import filecmp
+import threading
+
+def isBalanced(string):
+    stack = list()
+    for c in string:
+
+        # just add left facing characters to "stack"
+        if c == '[' or c == '{' or c == '(':
+            stack.append(c)
+
+        # empty list check
+        elif len(stack) == 0:
+            return "NO"
+        
+        # otherwsise check for balance
+        elif c == ']' and stack.pop() == '[':
+            pass
+        
+        elif c == ')' and stack.pop() == '(':
+            pass
+
+        elif c == '}' and stack.pop() == '{':
+            pass
+        
+        else:
+            return "NO"
+
+    # if stack empty here, we're good
+    if len(stack) == 0:
+        return "YES"
+    else:
+        return "NO"
+
+def main(inputFile, outputFile):
+    # do not use input() as this reads from sys.stdin directly
+    # we can't change sys.stdin across threads, sadly
+    # use the below instead
+
+        # reading
+            # for line in inputFile:
+            # inputFile.readLine()
+
+	        # split():	seperates into list
+	        # rstrip():	removes whitespace from input
+
+        # writing
+            # outputFile.write(<str> + '\n')
+            # be careful with whitespace!
+    
+    n = int(inputFile.readline())
+    for i in range(n):
+        string = inputFile.readline().rstrip()
+        result = isBalanced(string)
+        outputFile.write(result)
+        if i != n - 1:
+            outputFile.write('\n')
+
+    return
+
+
+# returns a list of files in the test directory for the problem
+def getFilePaths(relativeDirectory: str):
+    # get full path 
+    currentDirectory = os.path.dirname(__file__)
+    fullPath = os.path.join(currentDirectory, relativeDirectory)
+
+    # exit if the directory doesn't exist
+    if os.path.isdir(fullPath) == False:
+        print("Directory '" + relativeDirectory + "' does not exist. Closing", file = sys.stderr)
+        sys.exit(-1)
+
+    # get all the files in the folder
+    filePaths = list()
+    for filename in os.listdir(fullPath):
+        # add files paths to a list
+        filePath = os.path.join(fullPath, filename)
+        if os.path.isfile(filePath):
+            filePaths.append(filePath)
+
+    return filePaths
+
+def runLocalTests():
+    # run test cases from test cases
+    # requires ./test/input and ./test/output to exist with the same number of files
+    # since we're downloading them directly from HackerRank, this is a safe assumption
+    inputFiles = getFilePaths("tests/input")
+    expectedOutputFiles = getFilePaths("tests/output")
+
+    threads = list()
+    
+    for i in range(len(inputFiles)):
+        # open file for input/output from hackerank
+        inputFileName = inputFiles[i] 
+        expectedOutputFileName = expectedOutputFiles[i]
+        outputFileName = os.path.join(os.path.dirname(__file__), os.path.basename(expectedOutputFileName))
+        
+        # run in a new subprocess
+        t = threading.Thread(target=runTest, args=[inputFileName, expectedOutputFileName, outputFileName])
+        threads.append(t)
+
+    # run threads
+    for t in threads:
+        t.start()
+
+    # avoids closing stdin
+    for t in threads:
+        t.join()
+
+# accepts an open file parameter
+def runTest(inputFileName, expectedOutputFileName, outputFileName):
+    # open files
+    inputFile = open(inputFileName, mode="r")
+    outputFile = open(outputFileName, mode="w+")
+
+    # run main with a given stdin
+    main(inputFile, outputFile)
+
+    # close files
+    inputFile.close()
+    outputFile.close()
+
+    # compare files and print result to command line
+    printStr = "Test Case " + os.path.basename(inputFile.name) + ": "
+    if filecmp.cmp(expectedOutputFileName, outputFileName, shallow=True):
+        print(printStr + "PASSED")
+    else:
+        print(printStr + "FAILED")
+
+if __name__ == "__main__":
+    # if in my vscode enviornment
+    if len(sys.argv) > 1 and sys.argv[1] == "--jorgDebug":
+        runLocalTests()
+
+    # if not
+    else:
+        main(sys.stdin, sys.stdout)
